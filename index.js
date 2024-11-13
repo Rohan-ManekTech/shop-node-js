2938ee9db324948edcfd12a767e080265e1c7bc2
RDI <rohan.darji@manektech.com>
2024-11-13 09:09:07 +0530

    added .env file

--- index.js
+++ index.js
@@ -11,22 +11,21 @@ const stripeRoute = require("./routes/stripe");
 const cors = require("cors");
 
 dotenv.config();
-
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => console.log("DB Connection Successfull!"))
-  .catch((err) => {
-    console.log(err);
-  });
-
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successful"))
+  .catch((err) => {
+    console.error("DB Connection Error:", err);
+  });
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
